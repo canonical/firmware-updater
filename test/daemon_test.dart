7bb8b8369836f188a62e699f8915cffcc65d64f3
J-P Nurmi <jp.nurmi@canonical.com>
2022-09-09 14:21:52 +0200

    Share service mock via test utils
    It's a bit awkward to have it in the service test that would never
    ever use a service mock...

--- test/daemon_test.dart
+++ test/daemon_test.dart
@@ -5,7 +5,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:fwupd/fwupd.dart';
 import 'package:mockito/mockito.dart';
 
-import 'service_test.mocks.dart';
+import 'test_utils.mocks.dart';
 
 void main() {
   test('daemon properties', () async {
--- test/service_test.dart
+++ test/service_test.dart
@@ -1,8 +1,3 @@
 import 'package:firmware_updater/service.dart';
-import 'package:mockito/annotations.dart';
 
-// ignore: unused_import, (shared in other tests)
-import 'service_test.mocks.dart';
-
-@GenerateMocks([FwupdService])
 void main() {}
