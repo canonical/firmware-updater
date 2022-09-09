374f947c416982f0b405b4ee2c9e529631750550
J-P Nurmi <jp.nurmi@canonical.com>
2022-09-09 14:26:41 +0200

    Add service test stub

--- test/service_test.dart
+++ test/service_test.dart
@@ -1,3 +1,23 @@
 import 'package:firmware_updater/service.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:fwupd/fwupd.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
 
-void main() {}
+import 'service_test.mocks.dart';
+
+@GenerateMocks([FwupdClient])
+void main() {
+  test('connects and closes the fwupd client', () async {
+    final client = MockFwupdClient();
+    when(client.propertiesChanged).thenAnswer((_) => const Stream.empty());
+
+    final service = FwupdService(fwupd: client);
+
+    await service.init();
+    verify(client.connect()).called(1);
+
+    await service.dispose();
+    verify(client.close()).called(1);
+  });
+}
