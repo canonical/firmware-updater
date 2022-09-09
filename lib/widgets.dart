ea39845c62af140bb8bdc547caab430e036f5c0e
J-P Nurmi <jp.nurmi@canonical.com>
2022-09-09 13:11:19 +0200

    Add missing ReleaseCard to widgets.dart

--- lib/widgets.dart
+++ lib/widgets.dart
@@ -5,5 +5,6 @@ export 'src/widgets/device_panel_list.dart';
 export 'src/widgets/message_dialog.dart';
 export 'src/widgets/option_card.dart';
 export 'src/widgets/refresh_button.dart';
+export 'src/widgets/release_card.dart';
 export 'src/widgets/small_chip.dart';
 export 'src/widgets/status_bar.dart';
