48940757bcd290a751ed2ff6b621d715d05d0297
Dennis Loose <dennis.loose@canonical.com>
2022-09-14 15:05:44 +0200

    adjust DeviceBody to match new design more closely

--- lib/src/widgets/device_body.dart
+++ lib/src/widgets/device_body.dart
@@ -1,8 +1,10 @@
+import 'package:collection/collection.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:fwupd/fwupd.dart';
 
+import 'device_icon.dart';
 import 'release_dialog.dart';
 import 'small_chip.dart';
 
@@ -45,100 +47,145 @@ class DeviceBody extends StatelessWidget {
     return _buildPadding(Text(text));
   }
 
+  static Widget _buildIconRow(
+    BuildContext context,
+    String vendor,
+    String name,
+    String description,
+    IconData icon,
+  ) {
+    return Row(
+      children: [
+        Icon(
+          icon,
+          size: 64,
+        ),
+        const SizedBox(width: 16),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              vendor,
+              style: Theme.of(context).textTheme.labelLarge,
+            ),
+            Text(
+              name,
+              style: Theme.of(context).textTheme.headlineSmall,
+            ),
+            Text(
+              description,
+              style: Theme.of(context).textTheme.labelMedium,
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     final l10n = AppLocalizations.of(context);
     return Padding(
-      padding: const EdgeInsets.only(top: 4, left: 16, right: 16, bottom: 16),
+      padding: const EdgeInsets.all(16),
       child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
         children: [
           Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Table(
-                columnWidths: const {
-                  0: IntrinsicColumnWidth(),
-                  1: FixedColumnWidth(16),
-                  2: IntrinsicColumnWidth(),
-                },
-                children: [
-                  if (device.version != null)
-                    TableRow(children: [
-                      DeviceBody._buildHeader(context, l10n.version),
-                      const SizedBox.shrink(),
-                      DeviceBody._buildLabel(context, device.version!),
-                    ]),
-                  if (device.vendor != null)
-                    TableRow(children: [
-                      DeviceBody._buildHeader(context, l10n.vendor),
-                      const SizedBox.shrink(),
-                      DeviceBody._buildLabel(context, device.vendor!),
-                    ]),
-                  if (device.guid.isNotEmpty)
-                    TableRow(children: [
-                      DeviceBody._buildHeader(context, l10n.guid),
-                      const SizedBox.shrink(),
-                      DeviceBody._buildPadding(
-                          SelectableText(device.guid.first)),
-                    ]),
-                  if (device.guid.length > 1)
-                    for (final guid in device.guid.skip(1))
-                      TableRow(children: [
-                        DeviceBody._buildHeader(context, ''),
-                        const SizedBox.shrink(),
-                        DeviceBody._buildPadding(SelectableText(guid)),
-                      ]),
-                ],
+              DeviceBody._buildIconRow(
+                context,
+                device.vendor ?? '',
+                device.name,
+                device.summary ?? '',
+                DeviceIcon.fromName(device.icon.firstOrNull),
               ),
-              const SizedBox(width: 16),
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Wrap(
-                    spacing: 8,
-                    runSpacing: 8,
-                    alignment: WrapAlignment.end,
-                    children: [
-                      for (final flag in device.flags)
-                        SmallChip(text: describeEnum(flag)),
-                    ],
-                  ),
+              if (canVerify || releases.isNotEmpty) const Spacer(),
+              if (canVerify || releases.isNotEmpty)
+                ButtonBar(
+                  children: [
+                    if (canVerify)
+                      OutlinedButton(
+                        onPressed: onVerify,
+                        child: Text(l10n.verifyFirmware),
+                      ),
+                    if (releases.isNotEmpty)
+                      hasUpgrade
+                          ? ElevatedButton(
+                              onPressed: () => showReleaseDialog(
+                                context,
+                                device: device,
+                                releases: releases,
+                                onInstall: onInstall,
+                              ),
+                              child: Text(l10n.showUpdates),
+                            )
+                          : OutlinedButton(
+                              onPressed: () => showReleaseDialog(
+                                context,
+                                device: device,
+                                releases: releases,
+                                onInstall: onInstall,
+                              ),
+                              child: Text(l10n.showReleases),
+                            ),
+                  ],
                 ),
-              ),
             ],
           ),
-          if (canVerify || releases.isNotEmpty) const SizedBox(height: 16),
-          if (canVerify || releases.isNotEmpty)
-            ButtonBar(
-              children: [
-                if (canVerify)
-                  OutlinedButton(
-                    onPressed: onVerify,
-                    child: Text(l10n.verifyFirmware),
-                  ),
-                if (releases.isNotEmpty)
-                  hasUpgrade
-                      ? ElevatedButton(
-                          onPressed: () => showReleaseDialog(
-                            context,
-                            device: device,
-                            releases: releases,
-                            onInstall: onInstall,
-                          ),
-                          child: Text(l10n.showUpdates),
-                        )
-                      : OutlinedButton(
-                          onPressed: () => showReleaseDialog(
-                            context,
-                            device: device,
-                            releases: releases,
-                            onInstall: onInstall,
-                          ),
-                          child: Text(l10n.showReleases),
-                        ),
-              ],
-            ),
+          const SizedBox(height: 32),
+          Table(
+            columnWidths: const {
+              0: IntrinsicColumnWidth(),
+              1: FixedColumnWidth(16),
+              2: IntrinsicColumnWidth(),
+            },
+            children: [
+              if (device.version != null)
+                TableRow(children: [
+                  DeviceBody._buildHeader(context, l10n.currentVersion),
+                  const SizedBox.shrink(),
+                  DeviceBody._buildLabel(context, device.version!),
+                ]),
+              if (device.versionLowest != null)
+                TableRow(children: [
+                  DeviceBody._buildHeader(context, l10n.minVersion),
+                  const SizedBox.shrink(),
+                  DeviceBody._buildLabel(context, device.versionLowest!),
+                ]),
+              if (device.vendor != null)
+                TableRow(children: [
+                  DeviceBody._buildHeader(context, l10n.vendor),
+                  const SizedBox.shrink(),
+                  DeviceBody._buildLabel(context, device.vendor!),
+                ]),
+              if (device.guid.isNotEmpty)
+                TableRow(children: [
+                  DeviceBody._buildHeader(context, l10n.guid),
+                  const SizedBox.shrink(),
+                  DeviceBody._buildPadding(SelectableText(device.guid.first)),
+                ]),
+              if (device.guid.length > 1)
+                for (final guid in device.guid.skip(1))
+                  TableRow(children: [
+                    DeviceBody._buildHeader(context, ''),
+                    const SizedBox.shrink(),
+                    DeviceBody._buildPadding(SelectableText(guid)),
+                  ]),
+              if (device.flags.isNotEmpty)
+                TableRow(children: [
+                  DeviceBody._buildHeader(context, l10n.flags),
+                  const SizedBox.shrink(),
+                  DeviceBody._buildPadding(
+                      Text(describeEnum(device.flags.first)))
+                ]),
+              if (device.flags.length > 1)
+                for (final flag in device.flags.skip(1))
+                  TableRow(children: [
+                    DeviceBody._buildHeader(context, ''),
+                    const SizedBox.shrink(),
+                    DeviceBody._buildPadding(Text(describeEnum(flag)))
+                  ]),
+            ],
+          ),
         ],
       ),
     );
