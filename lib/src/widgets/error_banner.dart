58cad9de5df39d9df84f2c9fa0e51b1388a22dbd
Dennis Loose <dennis.loose@canonical.com>
2022-10-11 17:52:28 +0200

    add ErrorBanner

---
+++ lib/src/widgets/error_banner.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class ErrorBanner extends StatefulWidget {
+  const ErrorBanner({
+    required this.child,
+    this.message,
+    super.key,
+  });
+
+  final Widget child;
+  final String? message;
+
+  @override
+  State<ErrorBanner> createState() => _ErrorBannerState();
+}
+
+class _ErrorBannerState extends State<ErrorBanner> {
+  bool _visible = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        if (widget.message != null && _visible)
+          Container(
+            color: Theme.of(context).colorScheme.error,
+            child: Row(
+              children: [
+                Expanded(
+                  child: Text(
+                    widget.message!,
+                    style: Theme.of(context).textTheme.labelMedium!.copyWith(
+                          color: Theme.of(context).colorScheme.onError,
+                        ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                IconButton(
+                  onPressed: () => setState(() => _visible = false),
+                  icon: const Icon(Icons.close, size: 16),
+                  padding: EdgeInsets.zero,
+                  visualDensity: const VisualDensity(
+                    horizontal: -4,
+                    vertical: -4,
+                  ),
+                  color: Theme.of(context).colorScheme.onError,
+                )
+              ],
+            ),
+          ),
+        Expanded(child: widget.child),
+      ],
+    );
+  }
+}
