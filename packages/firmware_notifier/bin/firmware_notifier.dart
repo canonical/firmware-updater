99d61b281e3e00a11b49b0d09cdd90597d41980c
Dennis Loose <dennis.loose@canonical.com>
2022-10-20 21:07:28 +0200

    remove leftovers from standalone package template

--- packages/firmware_notifier/bin/firmware_notifier.dart
+++ packages/firmware_notifier/bin/firmware_notifier.dart
@@ -1,5 +1,5 @@
 import 'package:desktop_notifications/desktop_notifications.dart';
-import 'package:fwupd_notifier/firmware_notifier.dart';
+import 'package:firmware_notifier/firmware_notifier.dart';
 
 void main(List<String> arguments) async {
   final client = NotificationsClient();
